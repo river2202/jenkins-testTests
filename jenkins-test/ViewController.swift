f5179dc1050cf0a79eb10ff46e8293ad6f8d498b
sww <sww.2008.wws@gmail.com>
2016-08-16 21:45:15 +1000

    test project

---
+++ jenkins-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jenkins-test
+//
+//  Created by Vivi Sun on 16/08/2016.
+//  Copyright (c) 2016 River. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
